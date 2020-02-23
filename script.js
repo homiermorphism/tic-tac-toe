0372d950e9465d2facbac07da931fae968114ea4
Samantha <samanthahomier@yahoo.com>
2020-02-22 23:04:58 -0800

    added not so smart computer

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+const playerOne = () => {
+  let playerOneName = document.getElementById('player-one').value;
+  if (playerOneName === '') {
+    playerOneName = 'Player One';
+  }
+  return playerOneName;
+}
+
+const playerTwoChoice = () => {
+  let playerTwoOptions = document.querySelector("input[name='player-two']");
+
+  let choice = '';
+  if (playerTwoOptions.checked) {
+    choice = 'human';
+  }
+  else {
+    choice = 'computer';
+  }
+  return choice;
+}
+
+const changeButton = () => {
+  let button = document.getElementById('submit-two');
+  let choice = playerTwoChoice();
+
+  if (choice === 'human') {
+    button.innerHTML = 'Submit';
+  }
+
+  else if (choice === 'computer') {
+    button.innerHTML = 'Start Game';
+  }
+}
+
+const playerTwo = () => {
+  let choice = playerTwoChoice();
+
+  if (choice === 'human') {
+    let playerTwoName = document.getElementById('player-two').value;
+
+    if (playerTwoName === '') {
+      playerTwoName = 'Player Two';
+    }
+  }
+  else if (choice === 'computer') {
+    let playerTwoName = 'Computer';
+  }
+  return playerTwoName;
+}
+
+const current = () => {
+  let div = document.querySelector('.current-player');
+  let playerOneName = playerOne();
+  let playerTwoName = playerTwo();
+  let currentName = keepTrack();
+
+  if (currentName === 'player one') {
+    div.innerHTML = playerOneName + "'s Turn";
+  }
+  else if (currentName === 'player two') {
+    div.innerHTML = playerTwoName + "'s Turn";
+  }
+}
+
 const start = () => {
   let startButton = document.getElementById('start');
   startButton.classList.add('hide');
@@ -6,7 +70,7 @@ const start = () => {
   screenTwo.classList.remove('hide');
 }
 
-const screenTwo = () => {
+const submitOne = () => {
   let screenTwo = document.getElementById('screen-two');
   screenTwo.classList.add('hide');
 
@@ -14,52 +78,38 @@ const screenTwo = () => {
   screenThree.classList.remove('hide');
 }
 
-const screenThree = () => {
+const submitTwo = () => {
+  let form = document.getElementById('player-two-options');
+  form.classList.add('hide');
+
+  let button = document.getElementById('submit-two');
+  button.classList.add('hide');
+
+  let choice = playerTwoChoice();
+  if (choice === 'human') {
+    let playerTwoInfo = document.getElementById('human-player-two');
+    playerTwoInfo.classList.remove('hide');
+  }
+  else if (choice === 'computer') {
+    startGame();
+  }
+}
+
+const startGame = () => {
   let screenThree = document.getElementById('screen-three');
   screenThree.classList.add('hide');
 
   let boardContainer = document.querySelector('.board-container');
   boardContainer.classList.remove('hide');
-}
 
-
-const startGame = () => {
-  let gameBoard = document.querySelector('.game-board');
-  gameBoard.classList.remove('hide');
-
-  let startGameButton = document.getElementById('start-game');
-  startGameButton.classList.add('hide');
-
-  let newGameButton = document.getElementById('new-game');
-  newGameButton.classList.remove('hide');
-
-  playerOne();
-  playerTwo();
+  let sidebar = document.getElementById('sidebar');
+  sidebar.classList.remove('hide');
 }
 
 const newGame = () => {
   endGame();
   let gameBoard = document.querySelector('.game-board');
   gameBoard.classList.remove('hide');
-}
-
-const playerOne = () => {
-  let playerOneName = document.getElementById('player-one').value;
-  if (playerOneName === '') {
-    playerOneName = 'Player One';
-  }
-  let input = document.getElementById('input-one');
-  input.innerHTML = playerOneName;
-}
-
-const playerTwo = () => {
-  let playerTwoName = document.getElementById('player-two').value;
-
-  if (playerTwoName === '') {
-    playerTwoName = 'Player Two';
-  }
-  let input = document.getElementById('input-two');
-  input.innerHTML = playerTwoName;
 }
 
 let currentPlayerArray = [1];
@@ -81,13 +131,34 @@ const keepTrack = () => {
 let xDivs = [];
 let oDivs = [];
 
+const compChoice = () => {
+  let boardPieces = document.querySelectorAll('.board-piece');
+
+  let array = [];
+  for (i=0; i < 9; i++) {
+    if (boardPieces[i].innerHTML === '') {
+      array.push(i);
+    }
+  }
+  let choice = array[Math.floor(Math.random()*array.length)];
+  boardPieces[choice].click();
+}
+
 const placeMarker = (e) => {
   if (e.target.innerHTML === '') {
     let currentPlayer = keepTrack();
-    let players = document.querySelectorAll('.player');
+    let choice = playerTwoChoice();
+
     if (currentPlayer === 'player one') {
-      e.target.innerHTML = 'X';
-      xDivs.push(Number(e.target.id));
+      if (choice === 'human') {
+        e.target.innerHTML = 'X';
+        xDivs.push(Number(e.target.id));
+      }
+      else {
+        e.target.innerHTML = 'X';
+        xDivs.push(Number(e.target.id));
+        compChoice();
+      }
     }
     else if (currentPlayer === 'player two') {
       e.target.innerHTML = 'O';
@@ -113,8 +184,6 @@ const checkWinner = (element) => {
     if (winningCombos[i].every(check)) {
       alert('hi');
     }
-    console.log(winningCombos[i]);
-    console.log(winningCombos[i].every(check));
   }
 }
 
@@ -140,11 +209,10 @@ const endGame = () => {
 
 document.addEventListener('click', function(e) {
   if (e.target.id === 'start') start();
-  if (e.target.id === 'submit-one') screenTwo();
-  if (e.target.id === 'submit-two') screenThree();
-  if (e.target.id === 'start-game') startGame();
+  if (e.target.id === 'submit-one') submitOne();
+  if (e.target.id === 'submit-two') submitTwo();
+  if (e.target.id === 'submit-three') startGame();
   if (e.target.id === 'new-game') newGame();
   if (e.target.classList.contains('board-piece')) placeMarker(e);
-  if (e.target.id === 'submit-one') playerOne();
-  if (e.target.id === 'submit-two') playerTwo();
+  if (e.target.name === 'player-two') changeButton();
 });
